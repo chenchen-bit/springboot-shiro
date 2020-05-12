f357b88e32e5cdb019627bf5ca455cad7b61729c
chenchen <chenchen-x@qq.com>
2020-05-12 17:36:32 +0800

    update

---
+++ src/main/java/com/learn/shiro/domain/User.java
@@ -0,0 +1,13 @@
+package com.learn.shiro.domain;
+
+/**
+ * User
+ *
+ * @author chenchen
+ * @date 2020/05/12
+ */
+public class User {
+
+
+
+}
--- src/test/java/com/learn/shiro/SpringbootShiroApplicationTests.java
+++ src/test/java/com/learn/shiro/SpringbootShiroApplicationTests.java
@@ -1,13 +1,44 @@
 package com.learn.shiro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 测试类
+ *
+ * @author chenchen
+ * @date 2020/05/12
+ */
 @SpringBootTest
 class SpringbootShiroApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testMySQL() throws SQLException {
+        //根据配置的数据库信息获取连接，执行语句
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from sys_user");
+        //打印结果
+        while (resultSet.next()) {
+            int id = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            String address = resultSet.getString(3);
+            System.out.println("id:" + id + " name:" + name + " address:" + address);
+        }
     }
 
 }
